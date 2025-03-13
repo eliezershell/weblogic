c09a56d5a8fe6065f9b9e9a1813a920e91defc2f
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2025-03-13 18:29:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# dependências
 sudo apt update -y
-sudo apt install unzip -y
 
 # criar swap
 sudo fallocate -l 1G /swapfile
@@ -14,5 +11,4 @@ sudo swapon /swapfile
 sudo dpkg -i jdk-11.0.26_linux-x64_bin.deb
 
 # instalar weblogic
-unzip V1045131-01.zip
 java -jar fmw_14.1.2.0.0_wls.jar

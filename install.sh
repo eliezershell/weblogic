c777f6e54805ff21d08680145b24d8f09ca45ae7
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2025-03-13 18:09:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
 # dependências
-
 sudo apt update -y
 sudo apt install unzip -y
 
 # criar swap
-
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 
 # instalar oracle jdk
-
-sudo dpkg -i jdk-11.0.26_linux-64_bin.deb
-sudo mkdir -p /usr/lib/jvm
-sudo cp -r jdk-11.0.26-oracle-x64 /usr/lib/jvm 
-
-# nesse ponto o comando java -version deve estar exbindo a versão do java instalado
-# se não estiver funcionando deve-se usar o comando update-alternatives sugerido pelo chatgpt
-# também deve-se definir as versões do java no bashrc
+sudo dpkg -i jdk-11.0.26_linux-x64_bin.deb
 
 # instalar weblogic
 unzip fmw_14.1.1.0.0_wls_lite_slim_Disk1_1of1.zip
-java -jar fmw_14.1.1.0.0[...].jar
-
+java -jar fmw_14.1.1.0.0_wls_lite_quick_slim_generic.jar
